c2c1afa46f0103536579d366be9ed03b7986b0cb
Marco Bunge <mab@grannyandsmith.com>
2016-02-02 16:15:21 +0100

    Add relations

---
+++ src/Relations/ManyThroughMany.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *
+ * (c) Marco Bunge <marco_bunge@web.de>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ *
+ * Date: 02.02.2016
+ * Time: 15:56
+ *
+ */
+
+namespace Blast\Db\Relations;
+
+
+class ManyThroughMany extends AbstractRelation
+{
+
+    /**
+     * Save related entities
+     *
+     * @return mixed
+     */
+    public function save()
+    {
+        // TODO: Implement save() method.
+    }
+
+    /**
+     * Fetch related entities
+     *
+     * @return mixed
+     */
+    public function fetch()
+    {
+        // TODO: Implement fetch() method.
+    }
+}
