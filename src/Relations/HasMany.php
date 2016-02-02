7d6d89d46d35391de101ec005b7c32b02b16449c
Marco Bunge <mab@grannyandsmith.com>
2016-02-02 16:48:19 +0100

    commit empty

--- src/Relations/HasMany.php
+++ src/Relations/HasMany.php
@@ -6,34 +6,64 @@
  * For the full copyright and license information, please view the LICENSE.txt
  * file that was distributed with this source code.
  *
- * Date: 02.02.2016
- * Time: 15:56
+ * Date: 01.02.2016
+ * Time: 15:18
  *
  */
 
 namespace Blast\Db\Relations;
 
 
+use Blast\Db\Entity\EntityInterface;
+
 class HasMany extends AbstractRelation
 {
+    /**
+     * BelongsTo constructor.
+     * @param EntityInterface $entity current entity instance
+     * @param string|EntityInterface $foreignEntity Entity which have a key field in current entity
+     * @param string|integer|null $localKey Field name on current entity which matches up with foreign key of foreign entity
+     * @param string|integer|null $foreignKey name on foreign entity which matches up with local key of current entity
+     */
+    public function __construct(EntityInterface $entity, $foreignEntity, $localKey = null, $foreignKey = null)
+    {
+        //local config
+        $this->entity = $entity;
+        $this->localKey = $localKey;
+
+        //foreign config
+        $this->foreignEntity = $foreignEntity;
+        $this->foreignKey = $foreignKey;
+
+
+    }
 
     /**
-     * Save related entities
-     *
-     * @return mixed
+     * Save foreign entity and store value of foreign key into local key field
+     * @return EntityInterface
      */
     public function save()
     {
-        // TODO: Implement save() method.
+        $foreignEntity = $this->getForeignEntity();
+        $entity = $this->getEntity();
+        $entity->__set($this->getLocalKey(), $foreignEntity->__get($this->getForeignKey()));
+
+        //save foreign only if it has updates
+        if($this->isForeignEntityUpdate()){
+            $foreignEntity->getMapper()->save($foreignEntity);
+        }
+
+        return $foreignEntity;
     }
 
     /**
-     * Fetch related entities
+     * Fetch data from foreign entity, when value of foreign key matches up with value of local key
      *
-     * @return mixed
+     * @return EntityInterface|\Blast\Db\Entity\EntityInterface[]
      */
     public function fetch()
     {
-        // TODO: Implement fetch() method.
+        return $this->getForeignEntity()->getMapper()->findBy($this->getForeignKey(), $this->getEntity()->__get($this->getLocalKey()));
     }
+
 }
