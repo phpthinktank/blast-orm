ba0dd0fd46199ee032147215e6bf8c38ccfd1224
Marco Bunge <mab@grannyandsmith.com>
2016-02-10 07:19:55 +0100

    Add select and all to mapper interface

--- src/Orm/MapperInterface.php
+++ src/Orm/MapperInterface.php
@@ -12,6 +12,7 @@ namespace Blast\Db\Orm;
 use Blast\Db\Entity\CollectionInterface;
 use Blast\Db\Entity\EntityInterface;
 use Blast\Db\Factory;
+use Blast\Db\Query;
 use Doctrine\DBAL\Query\QueryBuilder;
 
 interface MapperInterface
@@ -39,9 +40,23 @@ interface MapperInterface
     /**
      *
      * @param $value
-     * @return CollectionInterface|EntityInterface|Query
+     * @return EntityInterface
      */
     public function find($value);
+
+    /**
+     * Get a collection of all entities
+     *
+     * @return array|CollectionInterface
+     */
+    public function all();
+
+    /**
+     * Get a statement and build a query. Table is already selected
+     * @param array $selects
+     * @return Query
+     */
+    public function select($selects = ['*']);
 
     /**
      * @param CollectionInterface|EntityInterface|array $entity
