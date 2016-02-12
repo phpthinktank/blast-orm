97329440a81d38a0c42c67cfbcda50757f36eca4
Marco Bunge <mab@grannyandsmith.com>
2016-02-12 12:06:11 +0100

    Introduce result object for query

---
+++ src/Result.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *
+ * (c) Marco Bunge <marco_bunge@web.de>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ *
+ * Date: 12.02.2016
+ * Time: 09:31
+ *
+ */
+
+namespace Blast\Db;
+
+
+use Blast\Db\Data\AccessorInterface;
+use Blast\Db\Data\MutatorInterface;
+use Blast\Db\Data\MutatorTrait;
+use Blast\Db\Entity\AccessorTrait;
+
+/**
+ * Class Result
+ * @package Blast\Db
+ */
+class Result extends ResultCollection implements AccessorInterface, MutatorInterface{
+
+    use AccessorTrait;
+    use MutatorTrait;
+
+}
---
+++ src/ResultCollection.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *
+ * (c) Marco Bunge <marco_bunge@web.de>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ *
+ * Date: 12.02.2016
+ * Time: 11:37
+ *
+ */
+
+namespace Blast\Db;
+
+
+use Blast\Db\Data\DataObject;
+
+class ResultCollection extends DataObject
+{
+    /**
+     * @var Query
+     */
+    private $query;
+
+    public function __construct(Query $query = null)
+    {
+        $this->query = $query;
+    }
+
+    /**
+     * @return Query
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
+
+}
