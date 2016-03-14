ed6415b4232a4d841ff721c433b7f8e1ab929cfd
Marco Bunge <mab@grannyandsmith.com>
2016-03-14 08:07:04 +0100

    Readd entity aware classes

---
+++ src/Entity/EntityAwareInterface.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * (c) Marco Bunge <marco_bunge@web.de>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ *
+ * Date: 23.02.2016
+ * Time: 10:20
+ *
+ */
+namespace Blast\Orm\Entity;
+
+interface EntityAwareInterface
+{
+    /**
+     * @return array|\stdClass|\ArrayObject|object
+     */
+    public function getEntity();
+    /**
+     * @param array|\ArrayObject|\stdClass|object $entity
+     * @return $this
+     */
+    public function setEntity($entity);
+}
